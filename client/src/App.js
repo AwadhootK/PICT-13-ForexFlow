1fef1b811491c8b2bc2ed0a4a00b8e3d8829a53f
Awadhoot <awadhootk6@gmail.com>
2023-11-23 11:29:04 +0530

    pushed

--- client/src/App.js
+++ client/src/App.js
@@ -1,194 +1,158 @@
 // App.js
-import React, { useState, useEffect } from "react";
-import CurrencyChart from "./Components/CurrencyChart.js";
-import ExchangeRateDisplay from "./Components/ExchangeDisplay.js"; // Import ExchangeRateDisplay
-import CurrencySelector from "./Components/CurrencySelector";
-import DurationSelector from "./Components/DurationSelector";
-import BarGraph from "./Components/BarGraph.js";
-import CountryList from "./Components/fetchCountryList.jsx";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import CurrencyChart from './Components/CurrencyChart.js'
+import ExchangeRateDisplay from './Components/ExchangeDisplay.js' // Import ExchangeRateDisplay
+import CurrencySelector from './Components/CurrencySelector'
+import DurationSelector from './Components/DurationSelector'
+import BarGraph from './Components/BarGraph.js'
+import CountryList from './Components/fetchCountryList.jsx'
+import './App.css'
 
-import CurrencyConverter from "./Components/CurrencyConverter.jsx";
+import CurrencyConverter from './Components/CurrencyConverter.jsx'
 
-const durations = ["Weekly", "Monthly", "Quarterly", "Yearly"];
+const durations = ['Weekly', 'Monthly', 'Quarterly', 'Yearly']
 const fetchDetailsData = async (
   startCountry,
   destinationCountry,
   startDate,
-  endDate
+  endDate,
+  setX,
+  setY
 ) => {
   try {
-    const apiUrl = "http://localhost:3000/getDetails";
+    const apiUrl = 'http://localhost:3000/getDetails'
 
     const params = {
       sourceCountry: startCountry,
       destinationCountry: destinationCountry,
       startDate: startDate,
-      endDate: endDate,
-    };
+      endDate: endDate
+    }
 
-    const queryString = new URLSearchParams(params).toString();
-    const fullUrl = `${apiUrl}?${queryString}`;
+    const queryString = new URLSearchParams(params).toString()
+    const fullUrl = `${apiUrl}?${queryString}`
 
     // Make a GET request using Fetch API
     fetch(fullUrl)
-      .then((response) => {
+      .then(response => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error('Network response was not ok')
         }
-        return response.json();
+        return response.json()
       })
-      .then((data) => {
-        var xAxis = data.data.map((item) => item.date);
-        var yAxis = data.data.map((item) => item.exchangeRate);
-        const axes = { xAxis, yAxis };
-        return axes;
+      .then(data => {
+        var xAxis = data.data.map(item => {
+          var temp = item.date.split('T')
+          return temp[0]
+        })
+        var yAxis = data.data.map(item => item.exchangeRate)
+        setX(xAxis)
+        setY(yAxis)
       })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error)
+      })
   } catch (error) {
-    console.error("Error fetching details data:", error.message);
-    throw error;
+    console.error('Error fetching details data:', error.message)
+    throw error
   }
-};
+}
 
-const generateRandomData = (length) => {
-  return Array.from({ length }, (_, index) => ({
-    label: `2023-11-${index + 1}`,
-    value: Math.random() * 10 + 80,
-  }));
-};
-
-const getChartDataForDuration = (duration) => {
-  // Function to generate data based on selected duration
-  switch (duration) {
-    case "Weekly":
-      return generateRandomData(7);
-    case "Monthly":
-      return generateRandomData(30);
-    case "Quarterly":
-      return generateRandomData(90);
-    case "Yearly":
-      return generateRandomData(365);
-    default:
-      return generateRandomData(7);
-  }
-};
-const DataList = ({ data }) => {
-  const dates = data.map((item) => item.date);
-  const exchangeRates = data.map((item) => item.exchangeRate);
-  const accuracy = data.map((item) => item.accurate);
-
-  console.log(dates);
-  console.log(exchangeRates);
-
-  return (
-    <div>
-      <h2>Dates:</h2>
-      <ul>
-        {dates.map((date) => (
-          <li key={date}>{date}</li>
-        ))}
-      </ul>
-
-      <h2>Exchange Rates:</h2>
-      <ul>
-        {exchangeRates.map((rate) => (
-          <li key={rate}>{rate}</li>
-        ))}
-      </ul>
-
-      <h2>Accuracy:</h2>
-      <ul>
-        {accuracy.map((acc) => (
-          <li key={acc}>{acc.toString()}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+// const getChartDataForDuration = duration => {
+//   switch (duration) {
+//     case 'Weekly':
+//       return generateRandomData(7)
+//     case 'Monthly':
+//       return generateRandomData(30)
+//     case 'Quarterly':
+//       return generateRandomData(90)
+//     case 'Yearly':
+//       return generateRandomData(365)
+//     default:
+//       return generateRandomData(7)
+//   }
+// }
 
 const App = () => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [xAxis, setXAxis] = useState([]);
-  const [yAxis, setYAxis] = useState([]);
-  const currencies = CountryList();
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+  const [xAxis, setXAxis] = useState([])
+  const [yAxis, setYAxis] = useState([])
+  const currencies = CountryList()
   const [chartData, setChartData] = useState({
     labels: [],
-    data: [],
-  });
-  const [firstCurrency, setFirstCurrency] = useState("U.S. dollar   (USD)");
-  const [secondCurrency, setSecondCurrency] = useState("Indian rupee   (INR)");
-  const [selectedDuration, setSelectedDuration] = useState("Weekly");
+    data: []
+  })
+  const [firstCurrency, setFirstCurrency] = useState('U.S. dollar   (USD)')
+  const [secondCurrency, setSecondCurrency] = useState('Indian rupee   (INR)')
+  const [selectedDuration, setSelectedDuration] = useState('Weekly')
 
   // useEffect(() => {
 
   //   // DataList(response)
   // }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const rawData = getChartDataForDuration(selectedDuration);
-      const filteredData = rawData.filter(
-        (dataPoint) =>
-          (!startDate || dataPoint.label >= startDate) &&
-          (!endDate || dataPoint.label <= endDate)
-      );
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //   const rawData = getChartDataForDuration(selectedDuration)
+  //   const filteredData = rawData.filter(
+  //     dataPoint =>
+  //       (!startDate || dataPoint.label >= startDate) &&
+  //       (!endDate || dataPoint.label <= endDate)
+  //   )
 
-      const labels = filteredData.map((dataPoint) => dataPoint.label);
-      const data = filteredData;
+  //   const labels = filteredData.map(dataPoint => dataPoint.label)
+  //   const data = filteredData
 
-      setChartData({ labels, data });
-    };
+  //   setChartData({ labels, data })
+  // }
 
-    fetchData();
+  // fetchData()
 
-    //   const fetchDetData = async () => {
-    //     try {
-    //       const response = await fetch("http://localhost:3000/getCountryList");
-    //       const respData = await response.json();
-    //       // Assuming respData.data is the array of currency strings
-    //       currencies = respData.data
-    //         .map((currencyString) => {
-    //           // Extract the currency code inside the brackets
-    //           const matches = currencyString.match(/\(([^)]+)\)/);
+  //   const fetchDetData = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:3000/getCountryList");
+  //       const respData = await response.json();
+  //       // Assuming respData.data is the array of currency strings
+  //       currencies = respData.data
+  //         .map((currencyString) => {
+  //           // Extract the currency code inside the brackets
+  //           const matches = currencyString.match(/\(([^)]+)\)/);
 
-    //           // If there is a match, return the captured group (currency code)
-    //           return matches ? matches[1] : null;
-    //         })
-    //         .filter(Boolean); // Filter out any null values
+  //           // If there is a match, return the captured group (currency code)
+  //           return matches ? matches[1] : null;
+  //         })
+  //         .filter(Boolean); // Filter out any null values
 
-    //       const { x, y } = await fetchDetailsData(
-    //         "",
-    //         "Bahrain dinar   (BHD)",
-    //         "2014-01-01",
-    //         "2014-01-05"
-    //       );
-    //       setXAxis(xAxis);
-    //       setYAxis(yAxis);
-    //       console.log(xAxis);
-    //       console.log(yAxis);
-    //     } catch (error) {
-    //       console.error("Error fetching details data:", error.message);
-    //       // Handle errors if needed
-    //     }
-    //   };
+  //       const { x, y } = await fetchDetailsData(
+  //         "",
+  //         "Bahrain dinar   (BHD)",
+  //         "2014-01-01",
+  //         "2014-01-05"
+  //       );
+  //       setXAxis(xAxis);
+  //       setYAxis(yAxis);
+  //       console.log(xAxis);
+  //       console.log(yAxis);
+  //     } catch (error) {
+  //       console.error("Error fetching details data:", error.message);
+  //       // Handle errors if needed
+  //     }
+  //   };
 
-    //   fetchDetData();
-  }, [selectedDuration, startDate, endDate]); // Empty dependency array to trigger fetching data only on mount
+  //   fetchDetData();
+  // }, [selectedDuration, startDate, endDate]) // Empty dependency array to trigger fetching data only on mount
 
   return (
-    <div className="dashboard">
-      <div className="first_grid">
+    <div className='dashboard'>
+      <div className='first_grid'>
         <CurrencyConverter></CurrencyConverter>
       </div>
-      <div className="second_grid">
-        <div className="line_graph">
+      <div className='second_grid'>
+        <div className='line_graph'>
           <h1>Exchange Rate Dashboard</h1>
-          <div className="currencies">
-            <div className="currencies1">
+          <div className='currencies'>
+            <div className='currencies1'>
               <CurrencySelector
                 currencies={currencies}
                 onSelectCurrency={setFirstCurrency}
@@ -196,7 +160,7 @@ const App = () => {
                 selectorNumber={1}
               />
             </div>
-            <div className="currencies1">
+            <div className='currencies1'>
               <CurrencySelector
                 currencies={currencies}
                 onSelectCurrency={setSecondCurrency}
@@ -204,7 +168,7 @@ const App = () => {
                 selectorNumber={2}
               />
             </div>
-            <div className="currencies1">
+            <div className='currencies1'>
               <DurationSelector
                 durations={durations}
                 onSelectDuration={setSelectedDuration}
@@ -216,19 +180,17 @@ const App = () => {
               />
             </div>
           </div>
-          <div className="currencies1">
+          <div className='currencies1'>
             <button
               onClick={async () => {
-                const { xAxis1, yAxis1 } = await fetchDetailsData(
+                await fetchDetailsData(
                   firstCurrency,
                   secondCurrency,
                   startDate,
-                  endDate
-                );
-                setXAxis(xAxis1);
-                console.log(xAxis1);
-                setYAxis(yAxis1);
-                console.log(yAxis1);
+                  endDate,
+                  setXAxis,
+                  setYAxis
+                )
               }}
             >
               Get Details
@@ -241,7 +203,7 @@ const App = () => {
             currencyName={secondCurrency}
             startDate={startDate}
             endDate={endDate}
-            selectedDuration={selectedDuration}
+            // selectedDuration={selectedDuration}
           />
           <ExchangeRateDisplay
             baseCurrency={firstCurrency}
@@ -249,7 +211,7 @@ const App = () => {
           />
         </div>
 
-        <div className="bar_graph">
+        <div className='bar_graph'>
           <BarGraph data={chartData.data} />
           <ExchangeRateDisplay
             baseCurrency={firstCurrency}
@@ -261,6 +223,6 @@ const App = () => {
         <CurrencyConverter></CurrencyConverter>
       </div> */}
     </div>
-  );
-};
-export default App;
+  )
+}
+export default App
--- client/src/Components/CurrencyChart.js
+++ client/src/Components/CurrencyChart.js
@@ -1,5 +1,4 @@
-// CurrencyChart.js
-import React from "react";
+import React from 'react'
 import {
   LineChart,
   Line,
@@ -7,34 +6,93 @@ import {
   YAxis,
   Tooltip,
   Legend,
-  ResponsiveContainer,
-} from "recharts";
+  ResponsiveContainer
+} from 'recharts'
 
-const CurrencyChart = ({ data, currencyName }) => {
+const CurrencyChart = ({
+  label,
+  data,
+  currencyName,
+  startDate,
+  endDate
+  // selectedDuration
+}) => {
+  if (!label) {
+    return 
+  }
+  const renderTooltip = value => [
+    value,
+    `Conversion Rate for 1 USD to ${currencyName}`
+  ]
+
+  // Filter data based on selected duration, start date, and end date
+  // const filteredData = data.filter(dataPoint => {
+  //   const dataDate = new Date(dataPoint.label)
+  //   const startDateObj = new Date(startDate)
+  //   const endDateObj = new Date(endDate)
+
+  //   switch (selectedDuration) {
+  //     case 'Weekly':
+  //       const startOfWeek = new Date(startDate)
+  //       const endOfWeek = new Date(endDate)
+  //       return dataDate >= startOfWeek && dataDate <= endOfWeek
+  //     case 'Monthly':
+  //       return (
+  //         dataDate.getFullYear() * 100 + dataDate.getMonth() + 1 >=
+  //           startDateObj.getFullYear() * 100 + startDateObj.getMonth() + 1 &&
+  //         dataDate.getFullYear() * 100 + dataDate.getMonth() + 1 <=
+  //           endDateObj.getFullYear() * 100 + endDateObj.getMonth() + 1
+  //       )
+  //     case 'Quarterly':
+  //       // Adjust this logic based on your specific definition of quarterly
+  //       const startQuarter =
+  //         Math.floor((startDateObj.getMonth() + 3) / 3) +
+  //         startDateObj.getFullYear() * 10
+  //       const endQuarter =
+  //         Math.floor((endDateObj.getMonth() + 3) / 3) +
+  //         endDateObj.getFullYear() * 10
+  //       const dataQuarter =
+  //         Math.floor((dataDate.getMonth() + 3) / 3) +
+  //         dataDate.getFullYear() * 10
+  //       return dataQuarter >= startQuarter && dataQuarter <= endQuarter
+  //     case 'Yearly':
+  //       return (
+  //         dataDate.getFullYear() >= startDateObj.getFullYear() &&
+  //         dataDate.getFullYear() <= endDateObj.getFullYear()
+  //       )
+  //     default:
+  //       return true
+  //   }
+  // })
+
+  const points = {}
+  for (let i = 0; i < label.length; i++) {
+    const dp =  {
+      label: label[i],
+      value: data[i]
+    }
+    points.push(dp)
+  }
+
+  console.log(points)
+
   return (
     <ResponsiveContainer width={700} height={212}>
-      <LineChart data={data}>
-        <XAxis dataKey="label" tick={{ fill: "#fff" }} />
-        <YAxis tick={{ fill: "#fff" }} />
-        <Tooltip
-          formatter={(value) => [
-            value,
-            `Conversion Rate for 1 USD to ${currencyName}`,
-          ]}
-        />
+      <LineChart data={points}>
+        <XAxis dataKey='label' />
+        <YAxis dataKey='value' />
+        <Tooltip formatter={renderTooltip} />
         <Legend />
         <Line
-          type="monotone"
-          dataKey="value"
-          stroke="rgb(102,195,172)"
+          type='monotone'
+          dataKey='value'
+          stroke='rgb(102,195,172)'
           strokeWidth={2}
-          dot={false} // Remove circles from the line chart
-          isAnimationActive={false} // Disable animation
           name={`Conversion Rate for 1 USD to ${currencyName}`}
         />
       </LineChart>
     </ResponsiveContainer>
-  );
-};
+  )
+}
 
-export default CurrencyChart;
+export default CurrencyChart
